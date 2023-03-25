f220afc95ecfa5dfdf34d1aac2a8ebedaada308f
Khaamid A Badriy <khaamidab@gmail.com>
2023-03-26 03:21:19 +0700

    update to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as helmet from '@fastify/helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { cors: true },
+  );
+  await app.register(helmet);
   await app.listen(3000);
 }
 bootstrap();
